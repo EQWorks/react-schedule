3934c629ecc638e7a7f98c8c7f46b02782d1900a
Selin Huang <selinhuang@Selins-MacBook-Pro.local>
2020-01-06 11:55:27 -0500

    GitHub Actions - build job for automated testing on push to local branch
    GitHub Actions - deploy to gh-pages
    - test push
    
    GitHub Actions - deploy job test push #2
    
    Github Actions - deployment job test push #3
    
    Github Actions - added test story to test automated deployment
    - test push #4
    
    Github Actions - deploy job test push #5
    
    Github Actions - deployment job test push #6
    
    Github Actions - deployment job test push #7
    
    Github Actions - deployment job test push #8
    
    Github Actions - test push #9
    
    Github Actions - modified storybook stories for deployment testing
    
    Github Actions - test deploy #10
    
    Github Actions - test deploy #11
    
    Github Actions - test deploy #12
    
    Github Actions - test deploy #13
    
    Github Actions - test deploy #14
    
    Github Actions - test push
    
    Github Actions - testing push with a new deployment action
    
    Github Actions - test push with build-and-deploy config format
    
    Github Actions - test deploy with new deploy action
    
    Github Actions - test deploy with deployment folder included in config
    
    Github Actions - test push with token
    
    Github Actions - changed folder param to root folder for deployment action
    
    Github Actions - changed deployment branch to gh-pages
    
    Github Actions - test push with homepage link added on package.json file
    
    Github Actions - test push with deployment folder changed to build
    
    Github Actions - test push without actions/checkout actions
    
    Github Actions - added github workspace path
    
    Github Actions - changed deployment script
    
    Github Actions - changed config format
    
    Github Actions - fixed config error
    
    Github Actions - test push
    
    Github Actions - test push #2
    
    Github Actions - test push
    
    Github Actions - test deploy
    
    Github Actions - test deploy/2
    
    Github Actions - specified gh token in package.json file;
    
    Github Actions - changed folder location to dist in deploy actions config
    
    Github Actions - changed github action for deployment
    - this actions requires github deploy key
    
    Github Actions - changed deploy config to using storybook-deployer tool
    - previously used github actions
    
    Github Actions - test run with config syntax error fix
    
    Github Actions - test run with deploy script change
    
    Github Actions - test run re-using github actions
    
    Github Actions - modified stories for test run
    
    Github Actions - test run with deploy dist directory as publish_dir
    
    Github Actions - test run
    
    Github Actions - test run with ./public directory added to workflow
    
    Github Actions - test run with config syntax error fix
    
    Github Actions - test run
    
    Github Actions - test run
    
    Github Actions - test run
    - recursive copy of stories directory to public directory in workflow
    
    Github Actions - test run

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,21 +1,200 @@
-import React from 'react'
+import React from "react";
 
-import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions';
-import 'semantic-ui-css/semantic.min.css'
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import "semantic-ui-css/semantic.min.css";
 
-import Schedule from '../src/schedule'
-
+import Schedule from "../src/schedule";
 
 const schedule = [
-  [false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false],
-  [false, false, false, false, false, false, false, false, true, true, true, true, true, true, true, true, true, true, true, false, false, false, false, false],
-]
+  [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+  ],
+  [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+  ],
+  [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+  ],
+  [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+  ],
+  [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+  ],
+  [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+  ],
+  [
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    false,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    true,
+    false,
+    false,
+    false,
+    false,
+    false
+  ]
+];
 
-storiesOf('Schedule', module)
-  .add('Simple', () => (<Schedule defaultSchedule={schedule} onChange={action('onChange')} />))
+storiesOf("Schedule", module)
+  .add("Simple", () => (
+    <Schedule defaultSchedule={schedule} onChange={action("onChange")} />
+  ))
+  .add("Simple-test", () => (
+    <Schedule defaultSchedule={schedule} onChange={action("onChange")} />
+  ));
